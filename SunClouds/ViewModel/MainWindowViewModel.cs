f5661e038005e7cbefe86f5fd3238e39ce420637
vanya <isip_i.i.evskin@mpt.ru>
2023-06-13 12:40:52 +0300

    test

--- SunClouds/ViewModel/MainWindowViewModel.cs
+++ SunClouds/ViewModel/MainWindowViewModel.cs
@@ -16,6 +16,20 @@ namespace SunClouds.ViewModel
 {
     internal class MainWindowViewModel
     {
-
+        public int Celsius0 { get; set; } = 11;
+        public int Feel0 { get; set; } = 11;
+        public string WeatherStatus0 { get; set; } = "НЕ Ясно.";
+        public int Celsius1 { get; set; } = 11;
+        public int Feel1 { get; set; } = 11;
+        public string WeatherStatus1 { get; set; } = "Ясно.";
+        public int Celsius2 { get; set; } = 11;
+        public int Feel2 { get; set; } = 11;
+        public string WeatherStatus2 { get; set; } = "Ясно.";
+        public int Celsius3 { get; set; } = 11;
+        public int Feel3 { get; set; } = 11;
+        public string WeatherStatus3 { get; set; } = "Ясно.";
+        public string Time1 { get; set; } = "11:11";
+        public string Time2 { get; set; } = "22:22";
+        public string Time3 { get; set; } = "33:33";
     }
 }
