c1988b78a00ea2062f52a2589e105dbb7d29c5d3
LEGION <andreisokolov190@gmail.com>
2023-06-14 18:50:47 +0300

    New page of settings and UserElement include.

---
+++ SunClouds/View/city_and_coord.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace end.View
+{
+    /// <summary>
+    /// Interaction logic for city_and_coord.xaml
+    /// </summary>
+    public partial class city_and_coord : UserControl
+    {
+        public city_and_coord()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SunClouds/View/page_day.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace end
+{
+    /// <summary>
+    /// Логика взаимодействия для page_day.xaml
+    /// </summary>
+    public partial class page_day : Page
+    {
+        public string cit_name_main { get; set; } = "Гиперборея";
+        public string cit_name_fav { get; set; } = "Гиперборея";
+        public string cit_coord { get; set; } = "90 ° 00 ′ 00 ″ .Долготы не имеет, так как является точкой схождения всех меридианов";
+        public page_day()
+        {
+            InitializeComponent();
+        }
+    }
+}
